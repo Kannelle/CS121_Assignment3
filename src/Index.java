0ba2fb5f150bdb7d3f80ab9d74ece718d6bfd59b
Kannelle <kristinawong54@gmail.com>
2016-02-19 15:20:00 -0800

    Frequency Class
    Copied the Frequency class from previous assignments to use in the
    multimap and updated the Index class to reflect this.

--- src/Index.java
+++ src/Index.java
@@ -9,16 +9,15 @@ public class Index {
 	// Takes as a parameter the location (directory) of all the files 
 	// and produces basic index as output.
 	// The index maps words to documents (pages).
-	public static Multimap<String, String> getIndex(String filePath) {
-		Multimap<String, String> multimap = ArrayListMultimap.create();
-		
+	public static Multimap<String, Frequency> getIndex(String filePath) {
+		Multimap<String, Frequency> multimap = ArrayListMultimap.create();
 		
 		//TODO
 		
 		// Find the number of documents
 		// Count the number of values per key in the multimap and find the sum
 		int valueCounter = 0;
-		for(String value : multimap.values()) {
+		for(Frequency value : multimap.values()) {
 		   valueCounter++;
 		  }
 		System.out.println("Documents: "+valueCounter);
@@ -29,11 +28,12 @@ public class Index {
 		System.out.println("Keys: "+size); 
 		
 		// Create a sample index: you can simply provide a text version of the key value pairs 
-		// You have in the indexes. Sample index is shown below.
+		// you have in the indexes.
 		
 		// Find the total size (in KB) of your index on disk
 		
-		//find the time taken to create your index
+		// Find the time taken to create your index
+		
 		return multimap;
 	}
 }
